11974df32e5e731c80398cf8236e1b5b290fd9c6
chamansuber <131013167+chamansuber@users.noreply.github.com>
2023-04-18 06:25:20 +0000

    Updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 
 function Footer() {
   return (
-    <div className='bg-black justify-center items-center w-full h-20 sm:h-30 flex flex-col mt-60'>
-        <p className='text-[#FFD854] sm:text-2xl'>&copy; Seaship Pvt. Ltd.</p>
-        <p className='text-[#FFD854] sm:text-2xl'>Developed By <Link href='https://pratiekdubey.github.io/' target='_blank'>Prateek Dubey</Link></p>
+    <div className='bg-black justify-center items-center w-full h-20 sm:h-40 flex flex-col sm:mt-56 mt-20'>
+        <p className='text-[#FFD854] sm:text-4xl'>&copy; Seaship Pvt. Ltd.</p>
+        <p className='text-[#FFD854] sm:text-4xl'>Developed By <Link href='https://pratiekdubey.github.io/' target='_blank'>Prateek Dubey</Link></p>
     </div>
   )
 }
